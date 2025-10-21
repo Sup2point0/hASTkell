eb7afbc59639cf9f7e57688be1675035a18a85b9
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2025-10-21 20:28:19 +0100

    dev - use `Double` for `Evaluator` computations

--- source/Evaluator.hs
+++ source/Evaluator.hs
@@ -1,9 +1,10 @@
 module Evalutator where
 
 
-evaluate :: (Tree String) -> Int
+evaluate :: (Tree String) -> Double
 evaluate (Leaf value)            = read value
 evaluate (Tree left Plus  right) = evaluate left + evaluate right
 evaluate (Tree left Minus right) = evaluate left - evaluate right
 evaluate (Tree left Mult  right) = evaluate left * evaluate right
 evaluate (Tree left Div   right) = evaluate left / evaluate right
+evaluate (Tree left Exp   right) = evaluate left ** evaluate right
