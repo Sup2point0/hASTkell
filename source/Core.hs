53a4fb12a7e6df112de822643cb0a22c276b746b
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2025-10-21 20:27:49 +0100

    fix - pad leading negative to allow starting lines with `-n`

--- source/Core.hs
+++ source/Core.hs
@@ -1,6 +1,14 @@
 module Core where
 
 
+sanitise :: String -> String
+sanitise = pad_negative . strip_spaces
+
+pad_negative :: String -> String
+pad_negative str@('-':_)
+  = '0' : str
+pad_negative str = str
+
 strip_spaces :: String -> String
 strip_spaces str
   = [char | char <- str, char /= ' ']
