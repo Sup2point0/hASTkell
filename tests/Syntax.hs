5fcf2beb67d4d8a431bb130cc99a2d3ea1543afe
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2025-10-23 14:21:43 +0000

    feat - implement `!==` assertion operator

--- tests/Syntax.hs
+++ tests/Syntax.hs
@@ -1,12 +1,16 @@
 module Syntax where
 
 import Test.Tasty
-import Test.Tasty.HUnit ((@?=), testCase, Assertion, HasCallStack)
+import Test.Tasty.HUnit
 
 
 infix 1 ===
 (===) :: (Show t, Eq t, HasCallStack) => t -> t -> Assertion
 (===) = (@?=)
+
+infix 1 !==
+(!==) :: (Show t, Eq t, HasCallStack) => t -> t -> Assertion
+(!==) prot deut = assertBool (show prot ++ " /= " ++ show deut) (prot /= deut)
 
 
 test_collection :: String -> [Assertion] -> TestTree
